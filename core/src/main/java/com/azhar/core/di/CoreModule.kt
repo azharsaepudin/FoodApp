1cdabc392d08484efd7dc8f3d685af0e96c98e7b
azharsaepudin <azharsaepudin5@gmail.com>
2021-01-22 22:09:49 +0700

    certificate pinning

--- core/src/main/java/com/azhar/core/di/CoreModule.kt
+++ core/src/main/java/com/azhar/core/di/CoreModule.kt
@@ -9,6 +9,7 @@ import com.azhar.core.data.source.remote.network.ApiService
 import com.azhar.core.domain.repository.IFoodAppRepository
 import com.azhar.core.utils.AppExecutors
 import com.google.gson.GsonBuilder
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,11 +20,20 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+
+        val hostname = "azharsaepudin.github.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/xlDAST56PmiT3SR0WdFOR3dghwJrQ8yXx6JLSqTIRpk=")
+            .add(hostname, "sha256/k2v657xBsOVe1PQRwOsHsw3bsGT2VzIqz5K+59sNQws=")
+            .add(hostname, "sha256/WoiWRyIOVNa9ihaBciRSC7XHjliYS9VwUGOIud4PB18=")
+            .build()
+
         OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .connectTimeout(120, TimeUnit.SECONDS)
-                .readTimeout(123, TimeUnit.SECONDS)
-                .build()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(123, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
     }
 
     single {
